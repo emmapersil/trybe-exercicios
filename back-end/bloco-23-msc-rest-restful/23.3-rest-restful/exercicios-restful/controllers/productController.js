e0719e665b7900c754c76d5d3b5c19dae6ef73c3
Emmanuelle Pereira <emma.persil@gmail.com>
2022-03-28 19:08:08 -0300

    Exercicios 4 feito, restful refactor!

--- back-end/bloco-23-msc-rest-restful/23.3-rest-restful/exercicios-restful/controllers/productController.js
+++ back-end/bloco-23-msc-rest-restful/23.3-rest-restful/exercicios-restful/controllers/productController.js
@@ -6,35 +6,53 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.json(products);
+  res.status(200).json(products);
 });
 
 router.get('/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.json(product);
+  if (!product) return res.status(404).json({ message: 'Produto não encontrado' });
+
+  res.status(200).json(product);
 });
 
 router.post('/', async (req, res) => {
   const { name, brand } = req.body;
-
+  try {
   const newProduct = await ProductModel.add(name, brand);
 
-  res.json(newProduct);
+  res.status(201).json(newProduct);
+
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({ message: 'Algo deu errado...'})
+  }
 });
 
 router.delete('/:id', async (req, res) => {
+
+  try {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.json(products);
+  res.status(200).json(products);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({ message: 'Algo deu errado...' })
+  }
 });
 
 router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
+  try {
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.json(products);
+  res.status(200).json(products);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({ message: 'Algo deu errado...' })
+  }
 });
 
 module.exports = router;
